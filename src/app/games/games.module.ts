ef3985d86855847e57c974e87e685032b4a69c3a
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-07-24 18:11:18 +0300

    модуль games

---
+++ src/app/games/games.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {GamesPageComponent} from './games-page/games-page.component';
+import {ViewPageComponent} from './view-page/view-page.component';
+
+@NgModule({
+    declarations: [
+      GamesPageComponent,
+      ViewPageComponent
+    ]
+  }
+)
+
+export class GamesModule {
+}
